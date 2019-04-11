48d83727e07dd65d334cafe3f0ad9c9b10ff9116
Oleksandr Maister <maister.alexander@gmail.com>
2019-04-12 01:28:13 +0300

    Add javascript.js and styles.css files

---
+++ javascript.js
@@ -0,0 +1,72 @@
+$(document).ready(function() {
+	$(".step-0").click(function() {
+		$(".step-0").css("color", "red");
+		$(".step-1").css("color", "black");
+		$(".step-2").css("color", "black");
+		$(".step-3").css("color", "black");
+		$(".step-4").css("color", "black");
+		$(".step-5").css("color", "black");
+	});
+
+	$(".step-1").click(function() {
+		$(".step-0").css("color", "black");
+		$(".step-1").css("color", "red");
+		$(".step-2").css("color", "black");
+		$(".step-3").css("color", "black");
+		$(".step-4").css("color", "black");
+		$(".step-5").css("color", "black");
+	});
+
+	$(".step-2").click(function() {
+		$(".step-0").css("color", "black");
+		$(".step-1").css("color", "black");
+		$(".step-2").css("color", "red");
+		$(".step-3").css("color", "black");
+		$(".step-4").css("color", "black");
+		$(".step-5").css("color", "black");
+	});
+
+	$(".step-3").click(function() {
+		$(".step-0").css("color", "black");
+		$(".step-1").css("color", "black");
+		$(".step-2").css("color", "black");
+		$(".step-3").css("color", "red");
+		$(".step-4").css("color", "black");
+		$(".step-5").css("color", "black");
+	});
+
+	$(".step-4").click(function() {
+		$(".step-0").css("color", "black");
+		$(".step-1").css("color", "black");
+		$(".step-2").css("color", "black");
+		$(".step-3").css("color", "black");
+		$(".step-4").css("color", "red");
+		$(".step-5").css("color", "black");
+	});
+
+	$(".step-5").click(function() {
+		$(".step-0").css("color", "black");
+		$(".step-1").css("color", "black");
+		$(".step-2").css("color", "black");
+		$(".step-3").css("color", "black");
+		$(".step-4").css("color", "black");
+		$(".step-5").css("color", "red");
+	});
+
+	$(".step-6").click(function() {
+		$(".step-0").css("color", "black");
+		$(".step-1").css("color", "black");
+		$(".step-2").css("color", "black");
+		$(".step-3").css("color", "black");
+		$(".step-4").css("color", "black");
+		$(".step-5").css("color", "black");
+		$(".step-6").css("color", "red");
+	});
+
+	$(".step-7").click(function() {
+		$(".step-0").css("color", "red");
+	});
+	$(".step-8").click(function() {
+		$(".step-0").css("color", "red");
+	});
+});
